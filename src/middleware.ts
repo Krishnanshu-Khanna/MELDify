23fe4092ff4cec11b64b6c6515e7bf0ebd8446ad
Krishnanshu <krishnanshu.khanna@gmail.com>
2025-06-19 00:35:06 +0530

    fix: update middleware to allow access to root path and refine route matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,11 @@ import { auth } from "./server/auth";
 export default auth((req) => {
   const isAuthenticated = !!req.auth;
 
+  // Allow access to "/" even if not authenticated
+  if (req.nextUrl.pathname === "/") {
+    return;
+  }
+
   if (!isAuthenticated) {
     const newUrl = new URL("/login", req.nextUrl.origin);
     return Response.redirect(newUrl);
@@ -10,5 +15,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/((?!_next|favicon.ico).*)"], // applies to all routes except static assets
 };
